ce43341ee30215b18030914ae722f106e7a62a7d
Eissa Purnell <eissamonet@gmail.com>
2026-01-21 17:53:40 -0600

    about section added

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,3 +1,37 @@
+import { Code2, Lightbulb, Rocket, Users } from "lucide-react";
+
+const highlights = [
+    { icon: Code2,
+      title: "Clean Code" ,
+      description: "I write clean, maintainable code following best practices and design patterns."
+    },
+    { icon: Rocket,
+      title: "Performace" ,
+      description: "I optimize applications for speed and efficiency to ensure a smooth user experience."
+    },
+    {
+      icon: Users,
+      title: "Collaboration" ,
+      description: "Work closely with teams to bring ideas to life."
+    },
+    {
+      icon: Lightbulb,
+      title: "Innovation" ,
+      description: "Staying ahead with the lated technologies and best practices."
+    }
+]
+
 export const About = () => {
-    return <section></section>;
+    return (
+     <section>
+        <div>
+          <div>
+            {/* left col */}
+            <div>
+              <span>About Me</span>
+            </div>
+          </div>
+        </div>
+     </section>
+    );
 };
