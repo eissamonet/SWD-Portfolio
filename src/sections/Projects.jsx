e65bd72448e423b413164776834b6064596412e4
Eissa Purnell <eissamonet@gmail.com>
2026-01-23 10:31:29 -0600

    project img added

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,3 +1,13 @@
+const projects = [
+  {
+    title: "Digital Agency",
+    description: "A modern digital agency website built with React and Tailwind CSS.",
+
+  }
+]
+
+
+
 export const Projects = () => {
     return <section></section>;
 };
