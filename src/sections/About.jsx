3ce3208e490034f38f5cb7184b4ed6d4dfd88e03
Eissa Purnell <eissamonet@gmail.com>
2026-01-25 19:49:34 -0600

    commit changes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -34,7 +34,7 @@ export const About = () => {
           {/* left col */}
           <div className="space-y-8">
             <div className="animate-fade-in">
-              <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase ">About Me</span>
+              <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">About Me</span>
             </div>
 
             <h2 className="text-4xl md:text-5xl font-bold leading-tight animate-fade-in animate-delay-100 text-secondary-foreground">
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -27,9 +27,9 @@ export const Experience = () => {
       <div className="containter mx-auto px-6 relative z-10">
         {/* section header */}
         <div className="max-w-3xl mb-16">
-          <span>Career Journey</span>
-          <h2>
-            Experince that <span> speaks volumes. </span>
+          <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">Career Journey</span>
+          <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
+            Experince that <span className="font-serif italic font-normal text-white"> {""} speaks volumes. </span>
           </h2>
         </div>
       </div>
