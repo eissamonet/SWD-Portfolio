5e86f650778327a118e04742bfeb1713cf05ad1a
Eissa Purnell <eissamonet@gmail.com>
2026-01-27 09:51:24 -0600

    commit changes

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -44,14 +44,16 @@ export const Experience = () => {
           {/* experience items */}
           <div className="space-y-12">
             {experiences.map((exp, index) => (
-              <div key={index}>
-
+              <div key={index}
+              className="relative grid md:grid-cols-2 gap-8 animate-fade-in"
+              style={{ animationDelay: `${(index + 1) * 100}ms`}}
+              >
                 {/* timeline item container */}
                 <div></div>
 
                 {/* content */}
-                <div>
-                  <div>
+                <div className={`pl-8 md:pl-0 ${index % 2 === 0 ? "md:pr-16 md:text-right" : "md:col-start-2 md:pl-16"} `}>
+                  <div className={`glass p-6 rounded-2xl border border-primary/30`}>
                     <span>{exp.period}</span>
                     <h3>{exp.role}</h3>
                     <p>{exp.company}</p>
