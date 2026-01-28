b9fdff82b10aed8efcd5a9e9d2a2eb784e8b4637
Eissa Purnell <eissamonet@gmail.com>
2026-01-27 20:51:09 -0600

    testimonials updated

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,3 +1,26 @@
+const testimonials = [
+    {
+        quote: "Working with this developer was an absolute pleasure. Their attention to detail and problem-solving skills are exceptional.",
+        author: "Mariana B",
+        role: "Product Manager",
+        image: /* insert image */
+    },
+    {
+        quote: "I've collaborated with many developers, but this one stands out for their professionalism and ability to deliver high-quality results.",
+        author: "Billy F",
+        role: "Software Engineer Lead",
+        image: /* insert image */
+    },
+    {
+        quote: "Their technical expertise and communication skills make them a valuable asset to any team. Highly recommended!",
+        author: "Kyle R",
+        role: "Software Engineer Lead",
+        image: /* insert image */
+    }
+]
+
+
+
 export const Testimonials = () => {
     return <section></section>;
 };
