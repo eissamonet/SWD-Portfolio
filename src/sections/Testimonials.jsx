6dfca378d379cc1516ad9564ba909e5ebbad9180
Eissa Purnell <eissamonet@gmail.com>
2026-01-27 20:58:57 -0600

    commit changes

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -3,24 +3,29 @@ const testimonials = [
         quote: "Working with this developer was an absolute pleasure. Their attention to detail and problem-solving skills are exceptional.",
         author: "Mariana B",
         role: "Product Manager",
-        image: /* insert image */
+        /* image:  insert image */
     },
     {
         quote: "I've collaborated with many developers, but this one stands out for their professionalism and ability to deliver high-quality results.",
         author: "Billy F",
         role: "Software Engineer Lead",
-        image: /* insert image */
+        /* image:  insert image */
     },
     {
         quote: "Their technical expertise and communication skills make them a valuable asset to any team. Highly recommended!",
         author: "Kyle R",
         role: "Software Engineer Lead",
-        image: /* insert image */
-    }
-]
+        /* image:  insert image */
+    },
+];
 
 
 
 export const Testimonials = () => {
-    return <section></section>;
+    return (
+       <section id="testimonials" className="py-32 relative overflow-hidden">
+         <div className="absolute top-1/2 left-1/2 w-[800px] h-[800px] bg-primary/5 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"
+         />
+       </section>
+    );
 };
