65836d2f15e81409cb6bc68502a1db72ec69c4c1
Eissa Purnell <eissamonet@gmail.com>
2026-01-27 21:13:16 -0600

    styling updated

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -26,6 +26,13 @@ export const Testimonials = () => {
        <section id="testimonials" className="py-32 relative overflow-hidden">
          <div className="absolute top-1/2 left-1/2 w-[800px] h-[800px] bg-primary/5 rounded-full blur-3xl -translate-x-1/2 -translate-y-1/2"
          />
+         <div className="container mx-auto px-6 relative z-10">
+            {/* section header */}
+            <div className="text-center max-w-3xl mx-auto mb-16">
+                <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in"> What People Say About Me</span>
+                <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in amimation-delay-100 text-secondary-foreground">Kind words from {""}</h2>
+            </div>
+         </div>
        </section>
     );
 };
