6efc53c227f33e0f65986c97f851687583762686
Eissa Purnell <eissamonet@gmail.com>
2026-01-29 11:43:32 -0600

    icons imported via lucid react

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,6 @@
- const contactInfo = [
+import { Mail, Phone, MapPin } from "lucide-react";
+
+const contactInfo = [
     {
        icon: Mail,
        label: "Email",
@@ -16,11 +18,22 @@
        label: "Location",
        value: "Chicago, IL",
        href: "#"
-    }
- ]
+    },
+ ];
 
 
 
 export const Contact = () => {
-    return <section></section>;
+    return (
+       <section id="contact" className="py-32 relative overflow-hidden">
+         <div className="absolute top-0 left-0 w-full h-full">
+            <div className=" absolute top-1/4 left-1/4 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
+            <div className="absolute bottom-1/4 right-1/4 w-64 h-64 bg-highlight/5 rounded-full blur-3xl" />
+         </div>
+
+         <div className="container mx-auto px-6 relative z-10">
+
+         </div>
+       </section>
+    );
 };
