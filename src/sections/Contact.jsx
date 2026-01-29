df89b0acf030a00c01d57c54c5aeaed7a8126782
Eissa Purnell <eissamonet@gmail.com>
2026-01-29 11:26:23 -0600

    contact section styling

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,3 +1,23 @@
+ const contactInfo = [
+    {  icon: Mail,
+       label: "Email",
+       value: "pedro@gmail.com",
+       href: "mailto:pedro@gmail.com",
+    },
+    {  icon: Phone,
+       label: "Phone",
+       value: "(555) 123-4567",
+       href: "tel:+15551234567",
+    },
+    {  icon: MapPin,
+       label: "Location",
+       value: "Los Angeles, CA",
+       href: "#"
+    }
+ ]
+
+
+
 export const Contact = () => {
     return <section></section>;
 };
