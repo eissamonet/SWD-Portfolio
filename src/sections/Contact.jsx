a93fe54a70df6575de9c25cd3811dd752977ff71
Eissa Purnell <eissamonet@gmail.com>
2026-01-30 10:33:57 -0600

    submit function added to button

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,5 +1,6 @@
 import { Mail, Phone, MapPin, Send } from "lucide-react";
 import { Button } from "../components/Button";
+import { useState } from "react";
 
 const contactInfo = [
     {
@@ -25,6 +26,17 @@ const contactInfo = [
 
 
 export const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+    }
+
     return (
        <section id="contact" className="py-32 relative overflow-hidden">
          <div className="absolute top-0 left-0 w-full h-full">
@@ -56,6 +68,10 @@ export const Contact = () => {
                       type="text"
                       required
                       placeholder="Your Name"
+                      value={formData.name}
+                      onChange={(e) =>
+                        setFormData({ ...formData, name: e.target.value})
+                      }
                       className="w-full px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all" />
                    </div>
 
@@ -67,6 +83,10 @@ export const Contact = () => {
                       <input
                       required
                       placeholder="Your Email"
+                      value={formData.email}
+                      onChange={(e) =>
+                        setFormData({ ...formData, email: e.target.value})
+                      }
                       className="w-full px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all" />
                    </div>
 
@@ -76,6 +96,10 @@ export const Contact = () => {
                       rows={5}
                       required
                       placeholder="Your Message"
+                      value={formData.message}
+                      onChange={(e) =>
+                        setFormData({ ...formData, message: e.target.value})
+                      }
                       className="w-full px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all resize-none" />
                    </div>
 
